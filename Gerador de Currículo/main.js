e9d685b6c5726bf0739f91d3d84a140ad5022211
LuisReiMamaco5 <132387272+LuisReiMamaco5@users.noreply.github.com>
2024-07-29 16:40:36 -0300

    Add files via upload

--- Gerador de Currículo/main.js
+++ Gerador de Currículo/main.js
@@ -1,3 +1,6 @@
+/*Tentativa de gerar currículo em PDF a partir das informações dadas pelo usuário*/
+/*Única biblioteca que foi testada e funcionou, não gerou resultados esperados, a divisão inteira do formulário foi para o PDF e está mal encaixada*/
+
 function gerarCurriculo(){
     const elemento = document.getElementById("formulario");
 
